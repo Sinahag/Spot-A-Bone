0185ba02ebad8b47bf96fd7a5b7dc79859793613
sinah <sinah@sfu.ca>
2022-11-09 11:01:33 -0800

    created menu to control spotify playback with play, queue, and read from NFC tag options

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-NEED TO RUN USING PYTHON3 ON DEBIAN
+### NEED TO RUN USING PYTHON3 ON DEBIAN
 
->> you can run the export_client.sh to initialize the environmental variables
->> need to specify your SPOTIPY_REDIRECT_URI as follows:
+you can run the export_client.sh to initialize the environmental variables
+need to specify your SPOTIPY_REDIRECT_URI as follows:
 export SPOTIPY_REDIRECT_URI='http://hostIP:8000/spotify/callback/'
 where hostIP is the IP of the system you're running on
--- main.py
+++ main.py
@@ -1,51 +1,59 @@
 import webbrowser
-from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import spotipy.util as util
+from NFCTAGURIMAPPING import getURI
+from userinfo import device_id, username
 
 from pprint import pprint
 import sys
-scope = 'user-library-read user-library-modify user-read-private playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing'
-username = 'justinmateo'
 
-# client_credentials_manager = SpotifyClientCredentials()
-# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+scope = 'user-top-read user-library-read user-library-modify user-read-private playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing'
 
 token = util.prompt_for_user_token(username, scope)
 
 if token:
     sp = spotipy.Spotify(auth=token)
 
-# if sys.argv[1]:
-#     results = sp.search(sys.argv[1], limit=1)
+if (sp.current_user()):
+    print(f"Current User: {sp.current_user()['id']}")
+    # sp.transfer_playback(device_id=device_id, force_play=True) # tries playing on iphone if it's available
+else:
+    print("Need an active session")
+    sys.exit()
 
-print(f"Current User: {sp.current_user()['id']}")
-# sp.start_playback()
-
-print(f"Currently Playing: {sp.current_playback()['item']['album']['name']}");
+print(f"Currently Playing: {sp.current_playback()['item']['name']}")
 
 while True:
-    print("Welcome to the project, " + sp.current_user()['display_name'])
-    print("0 - Exit the console")
+    print("SPOT-A-BONE running on:" + sp.current_user()['display_name'])
+    print("0 - Exit")
     print("1 - Search for a Song")
-    user_input = int(input("Enter Your Choice: "))
-    if user_input == 1:
+    print("2 - Read From NFC")
+    choice = int(input("choice: "))
+    if choice == 1:
         search_song = input("Enter the song name: ")
         results = sp.search(search_song, 1, 0, "track")
-        songs_dict = results['tracks']
-        song_items = songs_dict['items']
+        song_items = results['tracks']['items']
         song_name = song_items[0]['name']
         song_artist = song_items[0]['artists'][0]['name']
         song_id = song_items[0]['id']
         song_uri = song_items[0]['uri']
-        # sp.add_to_queue(song_id)
-        uris = list()
-        uris.append(song_uri)
-        sp.start_playback(uris=uris)
-        # sp.add_to_queue(song_id) # if you want to add
-        print(f"Playing {song_name} by {song_artist} on {sp.current_user()['id']} {sp.current_playback()['device']['name']}")
-    elif user_input == 0:
-        print("Good Bye, Have a great day!")
+        play_or_queue = str(input("Play Now (1) or Queue (any other key): "))
+        if play_or_queue=="1":
+            sp.start_playback(uris=[song_uri])
+            print(f"Playing {song_name} by {song_artist} on {sp.current_user()['id']} {sp.current_playback()['device']['name']}")
+        else:
+            sp.add_to_queue(song_id)
+            print(f"Queued {song_name} by {song_artist} on {sp.current_user()['id']} {sp.current_playback()['device']['name']}")
+    elif choice == 2:
+        uris=list()
+        with open('NFC.txt') as f:
+            for line in f:
+                uris.append(getURI(str(line.strip())))
+        if len(uris) > 0:
+            sp.start_playback(uris=uris)
+        open("NFC.txt", "w").close()
+        print(f"""Playing {sp.current_playback()['item']['name']} by {sp.current_playback()['item']['artists'][0]['name']} on {sp.current_user()['id']} {sp.current_playback()['device']['name']}""")
+    elif choice == 0:
         break
     else:
-        print("Please enter valid user-input.")
+        print("Invalid Input")
