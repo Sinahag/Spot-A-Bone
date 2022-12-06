a72fb312a01e789c9c6891b6658ba785c6bac830
sinah <sina_haghighi@sfu.ca>
2022-12-05 19:13:23 -0800

    new experiment

---
+++ experimental/connect_db.py
@@ -0,0 +1,131 @@
+import pyodbc
+import spotipy.util as util
+from userinfo import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+import sys
+
+def connect_db():
+    ODBC_STR = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:sfucmpt.database.windows.net,1433;Database=cmpt351-spotabone;Uid=sinah;Pwd=CMPT354#;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    return pyodbc.connect(ODBC_STR)
+
+class database:
+    def __init__(self,username):
+        self.conn = connect_db()
+        self.delete_old_tokens()
+        if(username):
+            self.username = username
+            self.userID=str()
+            self.get_user_id()
+            self.scope = 'user-top-read user-library-read user-library-modify user-read-private playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing'
+            self.token = str()
+            self.get_token()
+
+    def delete_old_tokens(self):
+        delete_old_tokens="DELETE FROM SPOTIFYTOKEN WHERE DATEDIFF(SECOND, TIME_CREATED, CURRENT_TIMESTAMP) > 3540"
+        cur = self.conn.cursor()
+        cur.execute(delete_old_tokens)
+        self.conn.commit()
+
+    def set_token(self):
+        insert_into_tokens_query = "INSERT INTO SPOTIFYTOKEN (TOKEN,USERNAME) VALUES (?,?);"
+        cur = self.conn.cursor()
+        cur.execute(insert_into_tokens_query,[self.token, self.userID])
+        self.conn.commit()
+
+    def get_token(self):
+        if self.token=="":
+            get_token_query = "SELECT TOKEN FROM SPOTIFYTOKEN WHERE username=? ORDER BY TIME_CREATED DESC"
+            cursor = self.conn.execute(get_token_query,self.userID)
+
+            self.token= cursor.fetchone()
+            if self.token:
+                self.token = self.token[0]
+
+            self.conn.commit()
+
+        if not self.token:  
+            self.token = util.prompt_for_user_token(username=self.userID, 
+                                                    scope = self.scope,
+                                                    client_id = SPOTIPY_CLIENT_ID,
+                                                    client_secret = SPOTIPY_CLIENT_SECRET,
+                                                    redirect_uri = SPOTIPY_REDIRECT_URI)
+            self.set_token()
+        return self.token
+
+    def get_users(self):
+        get_users_query = "SELECT username FROM USER_ID"
+        cursor = self.conn.execute(get_users_query)
+
+        users = cursor.fetchall()
+        filtered_users = list(filter(None, users))
+
+        self.conn.commit()
+        return filtered_users
+
+    def get_playlists(self):
+        get_playlists_query = "SELECT tagnum FROM NFC_PLAYLIST_URI"
+        cursor = self.conn.execute(get_playlists_query)
+
+        playlists = cursor.fetchall()
+        filtered_playlists = list(filter(None, playlists))
+
+        for i in range(len(filtered_playlists)):
+            filtered_playlists[i] = f"p{filtered_playlists[i]}"
+
+        self.conn.commit()
+        return filtered_playlists
+
+
+    def get_songs(self):
+        get_song_query = "SELECT tagnum FROM NFC_URI"
+        cursor = self.conn.execute(get_song_query)
+
+        songs = cursor.fetchall()
+        filtered_songs = list(filter(None, songs))
+
+        for i in range(len(filtered_songs)):
+            filtered_songs[i] = f"s{filtered_songs[i]}"
+
+        self.conn.commit()
+        return filtered_songs
+
+    def get_tag_uri(self,tagID):
+        get_uri_query = "SELECT uri FROM NFC_URI WHERE tagnum=?"
+        cursor = self.conn.execute(get_uri_query,tagID)
+
+        uri= cursor.fetchone()
+        uri=uri[0]
+
+        self.conn.commit()
+
+        return uri
+    
+    def get_playlist_id(self,playlistNum):
+        get_uri_query = "SELECT uri FROM NFC_PLAYLIST_URI WHERE tagnum=?"
+        cursor = self.conn.execute(get_uri_query,playlistNum)
+
+        uri= cursor.fetchone()
+        uri = uri[0]
+
+        self.conn.commit()
+
+        return uri
+
+    def get_user_id(self):
+        get_userID_query = "SELECT userID FROM USER_ID WHERE username=?"
+        cursor = self.conn.execute(get_userID_query,self.username)
+
+        self.userID = cursor.fetchone()
+        
+        if not self.userID:
+            print("User not registered")
+            sys.exit()
+
+        self.userID = self.userID[0]
+
+        self.conn.commit()
+
+        return self.userID
+
+    def cleanup(self):
+        self.conn.close()
+
---
+++ experimental/pipe.py
@@ -0,0 +1,108 @@
+import pipes
+import time
+import threading
+from connect_db import database
+import getpass
+# possible improvement is to get all the keys from the database at the beginning
+# and store them in dictionaries in here just so if we add more playlists etc, its fine
+
+class nfc_pipe:
+    def __init__(self):
+        print ("Creating pipe and thread...")
+
+        db = database(None)
+        self.commands = ['skip','track','pause','play','close']
+        self.users =db.get_users()
+        self.playlists = db.get_playlists()
+        self.songs = db.get_songs()
+        db.cleanup()
+
+        self.p = pipes.Template()
+        username = getpass.getuser()        
+        username = "/home/"+username
+        self.nfctags = username+ "/cmpt433/public/myApps/spotabone/comms/NFC.txt"
+        self.songNames = username+"/cmpt433/public/myApps/spotabone/comms/song.txt"
+
+        self.songQueue = list()
+        self.playlistQueue = list()
+        self.commandQueue = list()
+        self.userQueue = list()
+        self.searchSongQueue =  list()
+
+        self.read_tags_mutex = True # set true if you can read from tag, false if we're emptying buffer
+        self.nfcThread = threading.Thread(target=self.readNFC, args=(0.5,))
+        self.event = threading.Event()
+        self.nfcThread.start()
+
+    def getSongDirectory(self):
+        return self.songNames
+
+    def readNFC(self,delay):
+        while(1):
+            f = self.p.open(self.nfctags,'r')
+            try:
+                algo = f.readline()
+                while(algo):
+                    if(algo):
+                        algo = str(algo.strip())
+                        print (f"Tag {algo} found!\n")
+                        if (algo in self.commands):
+                            self.commandQueue.append(algo)
+                        elif (algo in self.users):
+                            self.userQueue.append(algo)
+                        elif (algo in self.playlists):
+                            algo = list(algo)
+                            algo = algo[1] # gets the playlist number
+                            self.playlistQueue.append(algo)
+                        elif (algo in self.songs):
+                            algo = list(algo)
+                            algo = algo[1] # gets the song number
+                            self.songQueue.append(algo)
+                        else:
+                            self.searchSongQueue.append(algo)
+                    algo = f.readline()
+            finally:
+                f.close()
+            open(self.nfctags, "w").close()
+            while(not self.read_tags_mutex):
+                time.sleep(delay)
+
+            if self.event.is_set():
+                break
+
+    def get_and_clear_commandBuffer(self):
+        if self.commandQueue:
+            return self.commandQueue.pop(0)
+
+        return None
+
+    def get_and_clear_userBuffer(self):
+        if self.userQueue:
+            return self.userQueue.pop(0)
+
+        return None
+
+    def get_and_clear_playlistBuffer(self):
+        if self.playlistQueue:
+            return self.playlistQueue.pop(0)
+
+        return None
+
+    def get_and_clear_songBuffer(self):
+        if self.songQueue:
+            return self.songQueue.pop(0)
+
+        return None
+
+    def get_and_clear_searchSongBuffer(self):
+        if self.searchSongQueue:
+            return self.searchSongQueue.pop(0)
+
+        return None
+
+    def close_pipe(self):
+        self.nfcThread.join()
+
+    def stop_thread(self):
+        self.event.set()
+    
